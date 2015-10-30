6622e745b89cd170b4950c2a6d5784bed00a1b2e
derekgroh <derekgroh@gmail.com>
2015-10-30 00:20:01 -0500

    rubocop changes that don't break code

--- resources/dnsserver.rb
+++ resources/dnsserver.rb
@@ -6,8 +6,8 @@
 actions :set
 default_action :set
 
-attribute :name, kind_of: 'String', name_attribute: true, required: true
-attribute :server_addresses, kind_of: 'String', regex: 'Resolv::IPv4::Regex'
-attribute :address_family, kind_of: 'String', default: 'IPv4'
-attribute :reset_server_addresses, kind_of: '[TrueClass, FalseClass]', default:
+attribute :name, kind_of: String, name_attribute: true, required: true
+attribute :server_addresses, kind_of: String, regex: Resolv::IPv4::Regex
+attribute :address_family, kind_of: String, default: 'IPv4'
+attribute :reset_server_addresses, kind_of: [TrueClass, FalseClass], default:
           false
